f6833f7ce392cde0c9bad335e7f715ef7c986675
williams <williams@kmandk.com>
2018-10-03 20:42:42 -0700

    Add project files.

---
+++ RealtorSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RealtorSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
